450bd51c4a258ec92da040b5c38e502d6bd699be
Rachel Katz <rachel.katz@flatironschool.com>
2022-10-20 10:56:11 -0400

    directors and actors

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div><h1>Actors</h1></div>;
+  return <div>
+    <h1>Actors</h1>
+    {actors.map((el) => {
+      return (
+        <div>
+          <h2>{el.name}</h2>
+          <ul>
+            {
+            el.movies.map((el) => <li>{el}</li>)
+            }
+          </ul>
+        </div>
+      )
+    })}
+  </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div><h1>Directors</h1></div>;
+  return <div>
+    <h1>Directors</h1>
+    {directors.map((el) => {
+      return (
+        <div>
+          <h2>{el.name}</h2>
+          <ul>
+            {
+            el.movies.map((el) => <li>{el}</li>)
+            }
+          </ul>
+        </div>
+      )
+    })}
+  </div>;
 }
 
 export default Directors;
