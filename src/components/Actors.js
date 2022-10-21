6513c4a3409fab916c14b5d78b8b80f6ad2a64c5
Rachel Katz <rachel.katz@flatironschool.com>
2022-10-21 09:53:32 -0400

    something

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Card, Icon, Image } from 'semantic-ui-react'
+
 import { actors } from "../data";
 
 function Actors() {
-  return <div><h1>Actors</h1></div>;
+  return <div>
+    <h1>Actors</h1>
+    {actors.map((el) => {
+      return (
+        <Card>
+          <Card.Content>
+            <Card.Header>{el.name}</Card.Header>
+            <Card.Description>
+              {
+              el.movies.map((el) => <li>{el}</li>)
+              }
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      )
+    })}
+  </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div><h1>Movies Page</h1></div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {
+      movies.map((el) => {
+        return (
+          <div>
+            <h2>{el.title}</h2>
+            <p>{el.time} minutes</p>
+            <ul>
+            {
+              el.genres.map((el) => <li>{el}</li>)
+            }
+            </ul>
+          </div>
+        )
+      })
+    }
+  </div>;
 }
 
 export default Movies;
